a159835accd3251f94c7686a1fc7bced8c1e207b
Dagur Ingi <daguringividar@gmail.com>
2025-03-14 09:39:03 +0000

    User and implementations completed

--- src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
+++ src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
@@ -1,18 +1,28 @@
 package is.hi.hbv202g.assignment8;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibrarySystem {
-    public LibrarySystem() {}
+    private final List<User> users = new ArrayList<>();
+
+    public LibrarySystem() {
+    }
 
     public void addBookWithTitleAndNameOfSingleAuthor(String title, String authorName) {
 
     }
 
-    public void addBookWithTitleAndAuthorList(String title, List<Author> authors) {
+    public void addBookWithTitleAndAuthorList(String title, List<Author> authors) throws EmptyAuthorListException {
 
     }
 
     public void addStudentUser(String name, boolean feePaid) {
-
+        if (feePaid) {
+            users.add(new User(name));
+        }
     }
 
     public void addFacultyMemberUser(String name, String department) {
@@ -27,7 +37,7 @@ public class LibrarySystem {
         return null;
     }
 
-    public void borrowBook(User user, Book book) {
+    public void borrowBook(User user, Book book) throws UserOrBookDoesNotExistException {
 
     }
 
@@ -35,7 +45,7 @@ public class LibrarySystem {
 
     }
 
-    public void returnBook(User user, Book book) {
+    public void returnBook(User user, Book book) throws UserPrincipalNotFoundException {
 
     }
 
--- src/main/java/is/hi/hbv202g/assignment8/UserOrBookDoesNotExistException.java
+++ src/main/java/is/hi/hbv202g/assignment8/UserOrBookDoesNotExistException.java
@@ -1,6 +1,6 @@
 package is.hi.hbv202g.assignment8;
 
-public class UserOrBookDoesNotExistException extends RuntimeException {
+public class UserOrBookDoesNotExistException extends Exception {
     public UserOrBookDoesNotExistException(String message) {
         super(message);
     }
