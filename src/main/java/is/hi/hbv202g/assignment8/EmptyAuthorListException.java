7a76cf19b9bcb3074bd14b5bc7e2084fe485532a
Dagur Ingi <daguringividar@gmail.com>
2025-03-14 09:18:03 +0000

    Classes created

---
+++ src/main/java/is/hi/hbv202g/assignment8/EmptyAuthorListException.java
@@ -0,0 +1,7 @@
+package is.hi.hbv202g.assignment8;
+
+public class EmptyAuthorListException extends RuntimeException {
+    public EmptyAuthorListException(String message) {
+        super(message);
+    }
+}
--- src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
+++ src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
@@ -1,3 +1,43 @@
 package is.hi.hbv202g.assignment8;
+
 public class LibrarySystem {
+    public LibrarySystem() {}
+
+    public void addBookWithTitleAndNameOfSingleAuthor(String title, String authorName) {
+
+    }
+
+    public void addBookWithTitleAndAuthorList(String title, List<Author> authors) {
+
+    }
+
+    public void addStudentUser(String name, boolean feePaid) {
+
+    }
+
+    public void addFacultyMemberUser(String name, String department) {
+
+    }
+
+    public Book findBookByTitle(String title) {
+        return null;
+    }
+
+    public User findUserByName(String name) {
+        return null;
+    }
+
+    public void borrowBook(User user, Book book) {
+
+    }
+
+    public void extendLending(FacultyMember facultyMember, Book book, LocalDate newDueDate) {
+
+    }
+
+    public void returnBook(User user, Book book) {
+
+    }
+
+
 }
